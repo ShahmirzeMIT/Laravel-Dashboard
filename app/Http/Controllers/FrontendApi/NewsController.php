7c788e48a5cb674c81c113849fba9f8e252d2523
Shahmirze <sahmirze.memmedyarov@gmail.com>
2023-10-01 10:53:02 +0400

    frontend is done

---
+++ app/Http/Controllers/FrontendApi/NewsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\FrontendApi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Backend\News;
+class NewsController extends Controller
+{
+    public function index(){
+        $news=News::all();
+        if($news->count()>0){
+            return response()->json([
+                'status'=>200,
+                'news'=>$news
+            ],200);
+        }
+        else{
+            return response()->json([
+                'status'=>200,
+                'news'=>"Don't have news"
+            ],404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/news',[App\Http\Controllers\FrontendApi\NewsController::class ,'index']);
 
 
